88db7511aeadd47e122170d822742dfc2c9e1531
qt333 <qt333@users.noreply.github.com>
2024-10-25 20:42:02 +0300

    add click on play button

--- core/clicker/blum.py
+++ core/clicker/blum.py
@@ -1,5 +1,7 @@
 import asyncio
+import time
 from itertools import product
+import pyautogui
 
 import keyboard
 import mouse
@@ -127,6 +129,20 @@ class BlumClicker:
 
         return False
 
+    @staticmethod
+    def click_on_play_button(screen: Any, rect: Tuple[int, int, int, int]
+    ) -> bool:
+        """
+        Click on the 'Play (nn left)' button.
+        """
+        width, height = screen.size
+        x, y = int(width * 0.5275), int(height * 0.8714)   # (211, 610) button center position for fullhd screen
+        screen_x = rect[0] + x
+        screen_y = rect[1] + y
+        mouse.move(screen_x, screen_y, absolute=True)
+        mouse.click(button=mouse.LEFT)
+        return True
+
     async def run(self) -> None:
         """
         Runs the clicker.
@@ -140,19 +156,28 @@ class BlumClicker:
             logger.info(get_language("p").format(window=window.title))
             logger.info(get_language("PRESS_S_TO_START"))
 
+            ft = 0
+            st = 0
             while True:
                 if await self.handle_input():
                     continue
-
+                if ft == 0:
+                    st = time.time()
                 rect = self.utils.get_rect(window)
 
                 screenshot = self.utils.capture_screenshot(rect)
 
                 self.collect_green(screenshot, rect)
-                self.collect_freeze(screenshot, rect)
+                # self.collect_freeze(screenshot, rect)
 
                 if get_config_value("COLLECT_DOGS"):
                     self.collect_dog(screenshot, rect)
+                #click play button after game time expire
+                ft = time.time() - st
+                if ft > 31:
+                    self.click_on_play_button(screenshot, rect)
+                    ft = 0
+                    continue
 
         except (Exception, ExceptionGroup) as error:
             logger.error(get_language("WINDOW_CLOSED").format(error=error))
